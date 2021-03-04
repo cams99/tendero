2bb5fe03e7b61313a360a40ea30c8cab1335013d
userlab-carlosmoreno <carlos.moreno@userlab.co>
2021-03-04 15:07:46 -0400

    Updates

---
+++ precache-manifest.4ce46d440241abca96ff2eea461992e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d34c307d319d5291f44a000ba432f882",
+    "url": "/tendero/index.html"
+  },
+  {
+    "revision": "409b8e0b1dbd55157cd4",
+    "url": "/tendero/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "97153f6bf2f53c7eb5dd",
+    "url": "/tendero/static/css/main.0727939f.chunk.css"
+  },
+  {
+    "revision": "409b8e0b1dbd55157cd4",
+    "url": "/tendero/static/js/2.fbf3c83e.chunk.js"
+  },
+  {
+    "revision": "26aa19e3344774568c18d9d87c8757a6",
+    "url": "/tendero/static/js/2.fbf3c83e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97153f6bf2f53c7eb5dd",
+    "url": "/tendero/static/js/main.16a8b689.chunk.js"
+  },
+  {
+    "revision": "4190f6b36d8081ea060c",
+    "url": "/tendero/static/js/runtime-main.e3b78264.js"
+  }
+]);
